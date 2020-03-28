2552e289562b337a61936bad30f484c566dcac06
Ian Armour <ianmarmour@gmail.com>
2020-03-28 04:35:30 -0700

    Update table name.

--- lib/muck_cdk-stack.ts
+++ lib/muck_cdk-stack.ts
@@ -5,7 +5,7 @@ export class MuckCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const table = new dynamodb.Table(this, "PLANT_TABLE", {
+    const table = new dynamodb.Table(this, "PLANTS_TABLE", {
       partitionKey: { name: "_id", type: dynamodb.AttributeType.STRING }
     });
   }
